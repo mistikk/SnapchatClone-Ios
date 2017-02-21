ddf82847cb29cc5ff5d1671f3f62650e53bf4a3e
mistikk <mustafa-buyukcelebi@hotmail.com>
2017-02-21 21:47:07 +0300

    init

---
+++ SnapchatClone/CameraVC.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SnapchatClone
+//
+//  Created by Mustafa Buyukcelebi on 21.02.2017.
+//  Copyright © 2017 Mustafa Buyukcelebi. All rights reserved.
+//
+
+import UIKit
+
+class CameraVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
+
